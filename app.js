fc1d9479b531d8281459357cf208396157e6807f
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-09 14:44:52 +0000

    small changes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require("express"),
     mongoose = require("mongoose"),
     validUrl = require("valid-url"),
     shortid = require("shortid");
-
+    
+    
 mongoose.connect("mongodb://localhost/url-shortener");
 app.set("view engine", "ejs");
+
 
 var urlSchema = new mongoose.Schema({
     
@@ -14,23 +16,23 @@ var urlSchema = new mongoose.Schema({
     
 });
 
-var Url = mongoose.model("Url", urlSchema);    
-    
+var Url = mongoose.model("Url", urlSchema);
+
 app.get("/",function(req,res){
     
     res.render("index");
-});    
+});
 
 app.get("/new/:url(*)",function(req,res){
-    var url = req.params.url ;
-    if(validUrl.isUri(url)){
+    var site = req.params.url ;
+        if(validUrl.isUri(site)){
         var code = shortid.generate();
-        var newUrl = {url:url,short:code};
+        var newUrl = { url:site , short:code };
         Url.create(newUrl,function(err,foundUrl){
            if(err){
                console.log(err);
            } else{
-                   res.json({original:url,new: foundUrl.short });
+                 res.json({original: site , new: foundUrl.short });
            }
         });
         
@@ -38,7 +40,10 @@ app.get("/new/:url(*)",function(req,res){
     else{
         res.json({ error: "Wrong url format, make sure you have a valid protocol and real site." });
     }
-});
+
+        });
+  
+
 
 app.get("/:short",function(req, res) {
    var short = req.params.short ;
@@ -55,5 +60,3 @@ app.get("/:short",function(req, res) {
 });
     
 app.listen(process.env.PORT, process.env.IP);
-
-
